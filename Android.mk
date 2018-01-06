037584ac8553dc91ff1cf88cdfb22af419194668
fezzy102 <fezzy102@gmail.com>
2018-01-06 11:09:50 -0500

    Get this building for Aquarios.

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-display))
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),caf-msm8998)
 
 sdm-libs := sdm/libs
 display-hals := include libqservice libqdutils $(sdm-libs)/utils $(sdm-libs)/core
@@ -25,5 +25,4 @@ ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,$(display-hals))
 endif
 endif
-
 endif
